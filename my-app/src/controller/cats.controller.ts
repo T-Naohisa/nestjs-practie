93d4ed6f85155565a86493633576d07f66db40ce
naohisa0820 <nao8.20y.peace@gmail.com>
2022-12-05 09:13:58 +0900

    dtoの追加、catsコントローラーをモジュールに追加

--- my-app/src/controller/cats.controller.ts
+++ my-app/src/controller/cats.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req, Body } from '@nestjs/common';
 import { FastifyRequest, FastifyReply } from 'fastify';
+import { CreateCatDto } from 'src/dto/cats.dto';
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
+  create(@Body() CreateCatDto: CreateCatDto): string {
     return 'This action adds a new cat';
   }
   @Get()
